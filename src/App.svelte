83fe06f711390b973e266e39c028644fed30caa7
Anders Sjöberg <anders.sjoberg@calabrio.com>
2020-01-28 11:23:12 +0100

    Prods

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,54 @@
 <script>
-	export let name;
+  export let name;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
+
+<div _ngcontent-iwa-c1="" class="container main-content">
+  <div class="row">
+    <div class="col-md-4">
+      <pm-product-list>
+        <div class="card">
+          <div class="card-header">Products</div>
+          <div class="card-body">
+            <div class="list-group">
+              <button class="list-group-item list-group-item-action rounded-0">
+                Leaf Rake (GDN-0011)
+              </button>
+              <button class="list-group-item list-group-item-action rounded-0">
+                Garden Cart (GDN-0023)
+              </button>
+              <button class="list-group-item list-group-item-action rounded-0">
+                Hammer (TBX-0048)
+              </button>
+              <button class="list-group-item list-group-item-action rounded-0">
+                Saw (TBX-0022)
+              </button>
+              <button class="list-group-item list-group-item-action rounded-0">
+                Video Game Controller (GMG-0042)
+              </button>
+            </div>
+          </div>
+          <div class="card-footer">
+            <div class="row">
+              <div class="form-check col-md-7">
+                <label>
+                  <input class="form-check-input" type="checkbox" />
+                  Display Product Code
+                </label>
+              </div>
+              <div class="col-md-4 text-right">
+                <button class="btn btn-primary">Add</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </pm-product-list>
+    </div>
+    <div class="col-md-8">
+      <pm-product-edit />
+    </div>
+  </div>
+</div>
